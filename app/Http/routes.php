336dfa28ef05a3c092dc01742364e8a957673b67
Rogant <avargas@mdsdigital.com>
2015-08-05 14:12:28 -0500

    get Facebook id

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,10 +10,10 @@
 |
 */
 
-Route::get('/', function (SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
-	$userFacebookID = 776627385;
-
-    return view('index')->with('userFacebookID', $userFacebookID);
+Route::any('/', function (SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
+	//$userFacebookID = 776627385;
+    //return view('index')->with('userFacebookID', $userFacebookID);
+    	echo '<script>window.location.assign("'.$fb->getLoginUrl(['email']).'")</script>';
 });
 
 Route::post('/registro', function () {
@@ -54,4 +54,68 @@ Route::get('gallery', function () {
 		->get();
 
     return view('gallery')->with('labels', $labels);
+});
+
+Route::get('/facebook/callback', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb)
+{
+    // Obtain an access token.
+    try {
+        $token = $fb->getAccessTokenFromRedirect();
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        dd($e->getMessage());
+    }
+
+    // Access token will be null if the user denied the request
+    // or if someone just hit this URL outside of the OAuth flow.
+    if (! $token) {
+        // Get the redirect helper
+        $helper = $fb->getRedirectLoginHelper();
+
+        if (! $helper->getError()) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        // User denied the request
+        dd(
+            $helper->getError(),
+            $helper->getErrorCode(),
+            $helper->getErrorReason(),
+            $helper->getErrorDescription()
+        );
+    }
+
+    if (! $token->isLongLived()) {
+        // OAuth 2.0 client handler
+        $oauth_client = $fb->getOAuth2Client();
+
+        // Extend the access token.
+        try {
+            $token = $oauth_client->getLongLivedAccessToken($token);
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    $fb->setDefaultAccessToken($token);
+
+    // Save for later
+    Session::put('fb_user_access_token', (string) $token);
+
+    // Get basic info on the user from Facebook.
+    try {
+        $response = $fb->get('/me?fields=id,name,email');
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        dd($e->getMessage());
+    }
+
+    // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
+    $facebook_user = $response->getGraphUser();
+
+    $query = DB::table('usuarios')->select('facebookID')->where('facebookID', '=', $facebook_user['id'])->first();
+
+    if($query){
+    	return redirect('gallery');
+    }else{
+    	return view('index')->with('userFacebookID', $facebook_user['id']);
+    }
 });
