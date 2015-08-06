a2905a015effb1f1e42cc632eeffc0e236f6300a
Rogant <avargas@mdsdigital.com>
2015-08-06 09:00:47 -0500

    funcionalidad Facebook

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,7 +23,7 @@ Route::any('/', function (SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
         $helper = $fb->getRedirectLoginHelper();
 
         if (! $helper->getError()) {
-            echo '<script>window.top.location.href = "'.$fb->getReRequestUrl(['email'], 'https://www.facebook.com/pages/Walking-Dead/178582138819465?sk=app_1616041778638428&app_data=fbAut').'";</script>';
+            echo '<script>window.top.location.href = "'.$fb->getLoginUrl(['email'], 'https://www.facebook.com/pages/Walking-Dead/178582138819465?sk=app_1616041778638428').'";</script>';
         }
 
         // User denied the request
@@ -49,6 +49,9 @@ Route::any('/', function (SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
 
     $fb->setDefaultAccessToken($token);
 
+    // Save for later
+    Session::put('fb_user_access_token', (string) $token);    
+
     // Get basic info on the user from Facebook.
     try {
         $response = $fb->get('/me?fields=id,name,email');
@@ -56,9 +59,17 @@ Route::any('/', function (SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
         dd($e->getMessage());
     }
 
-    // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
-    $facebook_user = $response->getGraphUser();    
-    return view('index')->with('userFacebookID', $facebook_user['id']);
+    $facebook_user = $response->getGraphUser();
+    Session::put('facebookID', $facebook_user['id']);
+
+
+    $query = DB::table('usuarios')->where('facebookID', '=', $facebook_user['id'])->count();
+
+    if($query > 0){
+        return redirect('gallery');
+    }else{
+        return view('index')->with('userFacebookID', $facebook_user['id']);
+    }
 });
 
 Route::post('/registro', function () {
@@ -108,72 +119,6 @@ Route::get('gallery', function () {
     $votos = DB::table('votos')->where('labelID', '=',  Session::get('facebookID'))->count();
 
     return view('gallery')->with('labels', $labels);
-});
-
-Route::get('/facebook/callback', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb)
-{
-    // Obtain an access token.
-    try {
-        $token = $fb->getAccessTokenFromRedirect();
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        dd($e->getMessage());
-    }
-
-    // Access token will be null if the user denied the request
-    // or if someone just hit this URL outside of the OAuth flow.
-    if (! $token) {
-        // Get the redirect helper
-        $helper = $fb->getRedirectLoginHelper();
-
-        if (! $helper->getError()) {
-            abort(403, 'Unauthorized action.');
-        }
-
-        // User denied the request
-        dd(
-            $helper->getError(),
-            $helper->getErrorCode(),
-            $helper->getErrorReason(),
-            $helper->getErrorDescription()
-        );
-    }
-
-    if (! $token->isLongLived()) {
-        // OAuth 2.0 client handler
-        $oauth_client = $fb->getOAuth2Client();
-
-        // Extend the access token.
-        try {
-            $token = $oauth_client->getLongLivedAccessToken($token);
-        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-            dd($e->getMessage());
-        }
-    }
-
-    $fb->setDefaultAccessToken($token);
-
-    // Save for later
-    Session::put('fb_user_access_token', (string) $token);
-
-    // Get basic info on the user from Facebook.
-    try {
-        $response = $fb->get('/me?fields=id,name,email');
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        dd($e->getMessage());
-    }
-
-    // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
-    $facebook_user = $response->getGraphUser();
-    Session::put('facebookID', $facebook_user['id']);
-
-
-    $query = DB::table('usuarios')->where('facebookID', '=', $facebook_user['id'])->count();
-
-    if($query > 0){
-    	return redirect('gallery');
-    }else{
-    	return view('index')->with('userFacebookID', $facebook_user['id']);
-    }
 });
 
 Route::post('ajaxVote', function () {
